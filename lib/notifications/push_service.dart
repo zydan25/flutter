b15413156e15e03a38ce0a6e4a1d7bf202286c5a
zydan25 <zydanalattab25w@gmail.com>
2026-08-17 19:51:35 +0300

    feat(notifications): add optional Firebase push notification router

---
+++ lib/notifications/push_service.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushService {
+  PushService({void Function(Map<String, dynamic>)? onAction}) : _onAction = onAction;
+
+  final void Function(Map<String, dynamic>)? _onAction;
+
+  Future<void> initialize() async {
+    try {
+      await Firebase.initializeApp();
+    } catch (_) {
+      // Firebase configuration is optional until google-services/GoogleService-Info is provided.
+    }
+    try {
+      await FirebaseMessaging.instance.requestPermission(alert: true, badge: true, sound: true);
+      FirebaseMessaging.onMessageOpenedApp.listen((message) => _dispatch(message.data));
+      FirebaseMessaging.onMessage.listen((message) => _dispatch(message.data));
+    } catch (_) {}
+  }
+
+  void _dispatch(Map<String, dynamic> data) {
+    final action = data['action'];
+    if (action is Map) _onAction?.call(action.cast<String, dynamic>());
+  }
+}
